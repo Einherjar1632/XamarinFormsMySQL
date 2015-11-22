902ccda2d7590d9ceb04d34600c6fead99794bbd
Daisuke Kobayashi <dreamers1632@gmail.com>
2015-11-22 15:57:55 +0900

    FirstCommit

---
+++ XamarinFormsMySQL/XamarinFormsMySQL/XamarinFormsMySQLPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using Xamarin.Forms;
+using I18N.West;
+
+namespace XamarinFormsMySQL
+{
+	public partial class XamarinFormsMySQLPage : ContentPage
+	{
+		public XamarinFormsMySQLPage ()
+		{
+			InitializeComponent ();
+		}
+
+        public void OnButtonClicked(object sender, EventArgs args)
+        {
+            try
+            {
+                //http://blog.snowwolf725.jjvk.com/2014/12/11/tutorial-how-to-connect-mysql-database-in-xamarin-android-ios/
+                new CP1250();
+
+                var cs = string.Format("Server=YourIPAddress;Port=3306;database=YourDatabase;User Id=YourID;Password=YourPassword;charset=utf8");
+                using(var db = new MySqlConnection(cs))
+                {
+                    db.Open();
+                    var queryString = "select hello from helloxamarin";
+                    var sqlcmd = new MySqlCommand(queryString, db);
+                    var result = sqlcmd.ExecuteScalar().ToString();
+                    LblMsg.Text = result;
+                    db.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                LblMsg.Text = ex.ToString();
+            }
+        }
+
+    }
+}
